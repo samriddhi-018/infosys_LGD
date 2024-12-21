275d1fcbc3c0765a827344432591ec1dc67245b4
samriddhi-018 <115361423+samriddhi-018@users.noreply.github.com>
2024-12-21 21:10:16 +0530

    commit

---
+++ LGD/app1/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Admin(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    admin_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
